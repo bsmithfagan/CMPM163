376416572074973645727a1452a1315e482c8488
bsmithfagan <bsmithfa@ucsc.edu>
2019-04-28 23:23:05 -0700

    build

--- HW1/Assets/Shaders/gameolife/PingPong_CellularAutomata.cs
+++ HW1/Assets/Shaders/gameolife/PingPong_CellularAutomata.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PingPong_CellularAutomata : MonoBehaviour
 {
     Texture2D texA;
     Texture2D texB;
-    Texture2D inputTex;
-    Texture2D outputTex;
+    //Texture2D inputTex;
+    //Texture2D outputTex;
     RenderTexture rt1;
+    RenderTexture rt2;
 
     Shader cellularAutomataShader;
     Shader ouputTextureShader;
@@ -34,52 +36,61 @@ public class PingPong_CellularAutomata : MonoBehaviour
 
         for (int i = 0; i < height; i++)
             for (int j = 0; j < width; j++)
-                    texA.SetPixel(i, j, new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1));
+                texA.SetPixel(i, j, new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1));
 
         texA.Apply(); //copy changes to the GPU
 
 
         rt1 = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
-   
+        rt2 = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        rt1.filterMode = FilterMode.Point;
+        rt2.filterMode = FilterMode.Point;
+
 
         rend = GetComponent<Renderer>();
 
         cellularAutomataShader = Shader.Find("Custom/RenderToTexture_CA");
         ouputTextureShader = Shader.Find("Custom/OutputTexture");
 
+        rend.material.shader = cellularAutomataShader;
+        Graphics.Blit(texA, rt1, rend.material);
+        Graphics.Blit(texB, rt2, rend.material);
     }
 
-   
+
     void Update()
     {
         //set active shader to be a shader that computes the next timestep
         //of the Cellular Automata system
         rend.material.shader = cellularAutomataShader;
-      
+
         if (count % 2 == 0)
         {
-            inputTex = texA;
-            outputTex = texB;
+            rend.material.SetTexture("_MainTex", rt1);
+            Graphics.Blit(rt1, rt2, rend.material);
+            rend.material.shader = ouputTextureShader;
+            rend.material.SetTexture("_MainTex", rt2);
         }
         else
         {
-            inputTex = texB;
-            outputTex = texA;
+            rend.material.SetTexture("_MainTex", rt2);
+            Graphics.Blit(rt2, rt1, rend.material);
+            rend.material.shader = ouputTextureShader;
+            rend.material.SetTexture("_MainTex", rt1);
+
         }
 
-
-        rend.material.SetTexture("_MainTex", inputTex);
-
+        /*
         //source, destination, material
-        Graphics.Blit(inputTex, rt1, rend.material);
-        Graphics.CopyTexture(rt1, outputTex);
+        //Graphics.Blit(inputTex, rt1, rend.material);
+        //Graphics.CopyTexture(rt1, outputTex);
 
 
         //set the active shader to be a regular shader that maps the current
         //output texture onto a game object
-        rend.material.shader = ouputTextureShader;
-        rend.material.SetTexture("_MainTex", outputTex);
-       
+        //rend.material.shader = ouputTextureShader;
+        //rend.material.SetTexture("_MainTex", rt1);
+        */
 
         count++;
     }
